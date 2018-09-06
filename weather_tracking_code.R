4c9ee60d03b54d570823e03af71d7c8791965d98
Kareem <kroger27@kent.edu>
2018-09-05 22:27:33 -0400

    change .jgp to .png and added different header and added sep= code

--- weather_tracking_code.R
+++ weather_tracking_code.R
@@ -1,7 +1,7 @@
-weather1<-read.csv(file.choose(), header=True)
+weather1<-read.csv(file.choose(), header=T, sep= ",")
 weather1
 summary(weather1)
 y <- weather1$Temp
-png (file = "line_chart.jpg")
+png (file = "line_chart.png")
 plot(y, type= "o", col = "green", xlab = "Days", ylab ="Degrees", main = " August Temperature chart")
 dev.off()
