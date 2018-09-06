44288d6605bebbbd140dfacf1692946f9bf71cc2
Kareem <kroger27@kent.edu>
2018-09-05 21:38:02 -0400

    changed the graph heading, and deleted old graph

--- weather_tracking_code.R
+++ weather_tracking_code.R
@@ -2,7 +2,6 @@ weather1<-read.csv(file.choose(), header=True)
 weather1
 summary(weather1)
 y <- weather1$Temp
-y1 <- weather1$Wind.speed..MPH.
 png (file = "line_chart.jpg")
-plot(y, type= "o", col = "green", xlab = "days", ylab ="degrees", main = " Temperature chart")
+plot(y, type= "o", col = "green", xlab = "Days", ylab ="Degrees", main = " August Temperature chart")
 dev.off()
